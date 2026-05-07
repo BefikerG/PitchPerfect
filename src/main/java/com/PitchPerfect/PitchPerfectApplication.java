005cb5f8d96f2ce5951bfb92b012f689f9da91cc
abeldevs1 <alta053125@gmail.com>
2026-05-07 21:25:26 +0300

    Initial commit: Spring Boot 3 application setup

---
+++ src/main/java/com/PitchPerfect/PitchPerfectApplication.java
@@ -0,0 +1,13 @@
+package com.PitchPerfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchPerfectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchPerfectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PitchPerfect/PitchPerfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PitchPerfect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchPerfectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
