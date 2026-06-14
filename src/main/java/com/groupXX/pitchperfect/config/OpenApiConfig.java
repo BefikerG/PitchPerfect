6e972ac39e6cd32d686446881fa5eecbdf9983ee
abeldevs1 <alta053125@gmail.com>
2026-06-14 18:45:28 +0300

    feat: add OpenAPI/Swagger documentation configuration

---
+++ src/main/java/com/groupXX/pitchperfect/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.groupXX.pitchperfect.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "PitchPerfect API", version = "v1", description = "Enterprise Booking Platform API"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
